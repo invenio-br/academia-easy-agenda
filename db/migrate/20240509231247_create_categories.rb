95d81959390324c8a3d8303cc592850ccde06c3a
Daniel Moreira <danieldenis01@gmail.com>
2024-05-14 21:54:38 -0300

    Cria Categoria

---
+++ db/migrate/20240509231247_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[7.1]
+  def change
+    create_table :categories do |t|
+      t.string :identifier, null: false
+      t.string :name, limit: 100
+
+      t.timestamps
+    end
+
+    add_index :categories, :identifier, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_010824) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_09_232325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "identifier", null: false
+    t.string "name", limit: 100
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["identifier"], name: "index_categories_on_identifier", unique: true
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name", limit: 100, null: false
@@ -21,6 +29,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_010824) do
     t.datetime "finished_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
+    t.string "url"
+    t.index ["category_id"], name: "index_events_on_category_id"
   end
 
+  add_foreign_key "events", "categories"
 end
