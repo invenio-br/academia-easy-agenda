29fe8fd4b8ec3411ff4cb7246a9790df6ee38647
Daniel Moreira <danieldenis01@gmail.com>
2024-05-20 23:23:06 -0300

    Adiciona Scopes e Enums

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,18 @@
 class Event < ApplicationRecord
+  enum status: { active: "active", removed: "removed" }
+
+  belongs_to :category
+
   validates :name, presence: true
   validates :started_at, presence: true
   validates :finished_at, presence: true
   validates :name, length: { minimum: 3, maximum: 100, allow_blank: true }
   validate :validate_if_starts_in_future, on: :create
   validate :validate_if_finished_greater_than_started
+
+  scope :with_category, -> { includes(:category) }
+  scope :today, -> { where(started_at: Date.current.beginning_of_day..Date.current.end_of_day) }
+  scope :in_period, ->(period_start, period_end) { where("started_at >= ? AND started_at <= ?", period_start, period_end) }
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_09_232325) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_234201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_232325) do
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
     t.string "url"
+    t.string "status", default: "active"
     t.index ["category_id"], name: "index_events_on_category_id"
+    t.index ["status"], name: "index_events_on_status"
   end
 
   add_foreign_key "events", "categories"
