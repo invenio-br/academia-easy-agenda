b3e0f7cfa9bed64392a1958375ea70a267cb79a4
Daniel Moreira <danieldenis01@gmail.com>
2024-05-28 19:21:28 -0300

    CRUD com Partials e Layouts

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -38,7 +38,7 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:identifier, :name)
   end
 
   def load_category
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :events
+
+  validates :identifier, presence: true
+  validates :name, presence: true
 end
