bf48c45738b03adafcca3ad28dbb27c68333fc31
Daniel Moreira <danieldenis01@gmail.com>
2024-06-10 20:27:06 -0300

    Insere I18n

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module EasyAgenda
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+    config.i18n.default_locale = :'pt-BR'
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
